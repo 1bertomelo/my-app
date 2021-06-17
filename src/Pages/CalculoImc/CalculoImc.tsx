48fd97b9a7143198fc0b05b48040404d2f7955a7
1bertomelo <1bertomelo@gmail.com>
2021-06-17 19:46:16 -0300

    exemplo imc solução

--- src/Pages/CalculoImc/CalculoImc.tsx
+++ src/Pages/CalculoImc/CalculoImc.tsx
@@ -7,8 +7,24 @@ export const CalculoImc = () => {
     const [resultadoImc, setResultadoImc] = React.useState<number>(0);
     const [classificacao, setClassificacao] = React.useState<string>('');
 
+
+    const calculoImc = (p: number, a: number) => {
+        return p / a ** 2;
+    }
+
     const handleCalcularImc = () => {
-        setResultadoImc(peso / altura ** 2);//Math.pow(altura,2);
+        setResultadoImc(calculoImc(peso, altura));//Math.pow(altura,2);
+        setClassificacao(classificarImc(calculoImc(peso, altura)));
+    }
+
+    const classificarImc = (pImc: number) => {
+        if (pImc < 18.5) return "Baixo Peso";
+        if (pImc >= 18.5 && pImc < 25) return "Peso Normal";
+        if (pImc >= 25 && pImc < 30) return "SobrePeso";
+        if (pImc >= 30 && pImc < 35) return "Obesidade I";
+        if (pImc >= 35 && pImc < 40) return "Obesidade II";
+
+        return "Obesidade III";
 
     }
 
@@ -30,7 +46,7 @@ export const CalculoImc = () => {
             <div>
                 <h3>Resultado</h3>
                 <h3>Imc: {resultadoImc.toFixed(2)}</h3>
-                <h3>Classificação: yyyy</h3>
+                <h3>Classificação: {classificacao}</h3>
             </div>
         </div>
     );
